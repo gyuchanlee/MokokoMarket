4853116d32d5357364e3199e9e53bfcf9ce9e25b
leegyuchan <hoo788@gmail.com>
2024-04-25 21:11:34 +0900

    boot backend first commit

---
+++ src/main/java/com/projectif/mokokomarket/MokokoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.projectif.mokokomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokokoMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MokokoMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectif/mokokomarket/MokokoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectif.mokokomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokokoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
