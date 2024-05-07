efd9c1498c16dab7dee27df5ad5ee3c05d654004
Lee Gyu Chan <hoo788@gmail.com>
2024-05-08 00:30:30 +0900

    feat: github actions test 38 - test 38

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
+# 빌드 환경 설정
 FROM openjdk:17-jdk-slim AS build
 WORKDIR /app
 
-# 프로젝트 빌드
+# 백엔드 빌드
 COPY gradlew .
 COPY gradle gradle
 COPY build.gradle .
@@ -14,17 +15,17 @@ RUN ./gradlew bootJar
 RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
 RUN apt-get update && apt-get install -y nodejs npm
 
-# React 빌드
+# 프론트엔드 빌드
 COPY /src/main/mokokofront mokokofront
 WORKDIR /app/mokokofront
 RUN npm install --force
 RUN npm run build
 
-# 최종 이미지 생성
+# 실행 환경 설정
 FROM openjdk:17-jdk-slim
 WORKDIR /app
 COPY --from=build /app/build/libs/*.jar app.jar
-COPY --from=build /app/mokokofront/build /static
+COPY --from=build /app/mokokofront/build ./static
 
 EXPOSE 8080
 
--- src/main/java/com/projectif/mokokomarket/global/WebMvcConfig.java
+++ src/main/java/com/projectif/mokokomarket/global/WebMvcConfig.java
@@ -13,34 +13,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .allowedMethods(HttpMethod.OPTIONS.name(), HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://localhost:3000")
+//                .allowCredentials(true)
+//                .allowedHeaders("*")
+//                .allowedMethods(HttpMethod.OPTIONS.name(), HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+//                .maxAge(3600);
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.noCache().cachePublic());
+                .addResourceLocations("classpath:/static/");
     }
-
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/")
-                .setViewName("forward:/index.html");
-
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        registry.addViewController("/**")
-                .setViewName("forward:/index.html");
-
-        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 }
